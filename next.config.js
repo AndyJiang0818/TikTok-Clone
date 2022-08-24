0e5017f679179e884b1b0af425510175cf76cf41
Xihao (Andy) Jiang <58491408+AndyJiang0818@users.noreply.github.com>
2022-08-24 14:17:13 -0400

    modified

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
